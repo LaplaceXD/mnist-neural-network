7c8ec8f82f80f7f05b7baadaf7c5418cc779afc1
Jonh Alexis Buot <ja.buotph@gmail.com>
2022-06-13 12:27:25 +0800

    [Feat] Added function for creating matrices

---
+++ lib/matrix/matrix.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "matrix.h"
+
+Matrix createMatrix(int row, int col)
+{
+    Matrix m = { NULL, row, col };
+    int idx;
+    
+    if(row <= 0 || col <= 0) {
+        fprintf(stderr, "Matrix creation failed. Invalid dimensions were supplied: %d x %d.", row, col);
+        exit(1);
+    }
+    
+    m.entries = (double **) malloc(row * sizeof(double*));
+    if(m.entries == NULL) {
+        fprintf(stderr, "Memory allocation failed.");
+        exit(1);
+    }
+    
+    for(idx = 0; idx < col; idx++) {
+        m.entries[idx] = (double *) malloc(col * sizeof(double));
+        if(m.entries[idx] == NULL) {
+            fprintf(stderr, "Memory allocation failed.");
+            exit(1);
+        }
+    }
+
+    return m; 
+}
+
