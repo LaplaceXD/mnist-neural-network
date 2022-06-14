c620dce7ab5a5c41f35c49635692ddb59e976dbd
Jonh Alexis Buot <ja.buotph@gmail.com>
2022-06-14 17:39:54 +0800

    [Feat] Updated main.c

--- main.c
+++ main.c
@@ -1,8 +1,30 @@
 #include <stdio.h>
-#include "lib/matrix/matrix.h"
+#include "lib/headers/mnist_img.h"
+#include "lib/headers/data.h"
+#include "lib/headers/matrix.h"
 
-int main()
+int main(int argc, char **argv)
 {
-    printf("Hello World!"); 
+    const MnistMetadata trainData = TRAIN_DATA;
+    
+    int idx;
+
+    Image imgs[trainData.imgCount];
+    readMnistCSV(imgs, trainData);
+    
+    printf("=================%d : %d ================\n", 1, imgs[0].value);
+    printMatrix(imgs[0].pixels);
+    printf("\n");
+
+    batchTransformImages(imgs, trainData.imgCount, standardizeValues);
+    
+    printf("=================%d : %d ================\n", 1, imgs[0].value);
+    printMatrix(imgs[0].pixels);
+    printf("\n");
+
+    for(idx = 0; idx < trainData.imgCount; idx++) {
+        freeMatrix(&imgs[idx].pixels);
+    }
+
     return 0;
 }
