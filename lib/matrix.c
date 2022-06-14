df95c521cf3f8683cf404d86df04006294890f4b
Jonh Alexis Buot <ja.buotph@gmail.com>
2022-06-14 20:09:12 +0800

    [Feat] Added ability to create 0 x 0 matrices

--- lib/matrix.c
+++ lib/matrix.c
@@ -17,30 +17,32 @@ Matrix createMatrix(int row, int col)
 {
     Matrix m = { NULL, row, col };
     int idx;
-    
-    if(row <= 0 || col <= 0) {
+
+    if (row == 0 && col == 0) {
+        m.entries = NULL;
+    } else if(row <= 0 || col <= 0) {
         fprintf(
             stderr,
             "Matrix creation failed. Invalid dimensions were supplied: %d x %d.",
             row, col
         );
         exit(1);
-    }
-    
-    m.entries = (double **) malloc(row * sizeof(double*));
-    if(m.entries == NULL) {
-        fprintf(stderr, "Memory allocation failed.");
-        exit(1);
-    }
-    
-    for(idx = 0; idx < row; idx++) {
-        m.entries[idx] = (double *) malloc(col * sizeof(double));
-        if(m.entries[idx] == NULL) {
+    } else {
+        m.entries = (double **) malloc(row * sizeof(double*));
+        if(m.entries == NULL) {
             fprintf(stderr, "Memory allocation failed.");
             exit(1);
         }
+        
+        for(idx = 0; idx < row; idx++) {
+            m.entries[idx] = (double *) malloc(col * sizeof(double));
+            if(m.entries[idx] == NULL) {
+                fprintf(stderr, "Memory allocation failed.");
+                exit(1);
+            }
+        }
     }
-
+    
     return m; 
 }
 
