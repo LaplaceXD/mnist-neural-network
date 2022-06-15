5b4db1a2400838bb299066a99959444ce4f69405
Jonh Alexis Buot <ja.buotph@gmail.com>
2022-06-15 16:09:49 +0800

    [Feat] Prepped main function

--- main.c
+++ main.c
@@ -1,30 +1,40 @@
 #include <stdio.h>
+#include <math.h>
+#include <time.h>
 #include "lib/headers/mnist_img.h"
-#include "lib/headers/data.h"
-#include "lib/headers/matrix.h"
+#include "lib/headers/neural_net.h"
 
 int main(int argc, char **argv)
 {
-    const MnistMetadata trainData = TRAIN_DATA;
-    
-    int idx;
+    srand((unsigned int) time(NULL)); // randomizer
+    // Load Datasets
+    printf("Loading Datasets...");
+    Image trainingImgs[TRAIN_DATA.imgCount], testingImgs[TEST_DATA.imgCount];
+    readMnistCSV(trainingImgs, TRAIN_DATA);
+    readMnistCSV(testingImgs, TEST_DATA);
+    printf(" DONE\n");
 
-    Image imgs[trainData.imgCount];
-    readMnistCSV(imgs, trainData);
-    
-    printf("=================%d : %d ================\n", 1, imgs[0].value);
-    printMatrix(imgs[0].pixels);
-    printf("\n");
+    // Normalize Datasets
+    printf("Normalizing Datasets...");
+    batchTransformImages(trainingImgs, TRAIN_DATA.imgCount, normalizeValues);
+    batchTransformImages(testingImgs, TEST_DATA.imgCount, normalizeValues);
+    printf(" DONE\n");
 
-    batchTransformImages(imgs, trainData.imgCount, normalizeValues);
-    
-    printf("=================%d : %d ================\n", 1, imgs[0].value);
-    printMatrix(imgs[0].pixels);
-    printf("\n");
+    // Create Neural Network
+    printf("Creating Neural Network...");
+    LayerDesign layers[] = {{IMG_SIZE, INPUT}, {16, HIDDEN}, {16, HIDDEN}, {10, OUTPUT}};
+    NeuralNetOpt opt = createNeuralNetOpt(HE_XAVIER, 1, 1);
+    NeuralNetwork nn = createNeuralNet(opt, layers, 4);
+    printf(" DONE\n");
 
-    for(idx = 0; idx < trainData.imgCount; idx++) {
-        freeMatrix(&imgs[idx].pixels);
-    }
+    // WIP
+
+    // Cleanup
+    printf("Cleaning up...");
+    freeNeuralNet(&nn);
+    freeImages(trainingImgs, TRAIN_DATA.imgCount);
+    freeImages(testingImgs, TEST_DATA.imgCount);
+    printf(" DONE\n");
 
     return 0;
 }
