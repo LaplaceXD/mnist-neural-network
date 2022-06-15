e01ae24c8940b540a2ef5912b84ab5bbf924fd03
Jonh Alexis Buot <ja.buotph@gmail.com>
2022-06-16 01:16:26 +0800

    [Misc] Prepped main.c

--- main.c
+++ main.c
@@ -1,40 +1,39 @@
 #include <stdio.h>
 #include <math.h>
 #include <time.h>
-#include "lib/headers/mnist_img.h"
+#include "lib/headers/util.h"
+#include "lib/headers/image_set.h"
 #include "lib/headers/neural_net.h"
 
 int main(int argc, char **argv)
 {
-    srand((unsigned int) time(NULL)); // randomizer
-    // Load Datasets
-    printf("Loading Datasets...");
-    Image trainingImgs[TRAIN_DATA.imgCount], testingImgs[TEST_DATA.imgCount];
-    readMnistCSV(trainingImgs, TRAIN_DATA);
-    readMnistCSV(testingImgs, TEST_DATA);
-    printf(" DONE\n");
-
-    // Normalize Datasets
-    printf("Normalizing Datasets...");
-    batchTransformImages(trainingImgs, TRAIN_DATA.imgCount, normalizeValues);
-    batchTransformImages(testingImgs, TEST_DATA.imgCount, normalizeValues);
-    printf(" DONE\n");
-
-    // Create Neural Network
-    printf("Creating Neural Network...");
+    srand((unsigned int) time(NULL)); // initialize randomizer
+ 
     LayerDesign layers[] = {{IMG_SIZE, INPUT}, {16, HIDDEN}, {16, HIDDEN}, {10, OUTPUT}};
-    NeuralNetOpt opt = createNeuralNetOpt(HE_XAVIER, 1, 1);
+    NeuralNetOpt opt = { .distStrat = HE_XAVIER, .distSize = 1, .initialBias = 1 };
     NeuralNetwork nn = createNeuralNet(opt, layers, 4);
-    printf(" DONE\n");
+    
+    /* =============== TRAINING ================== */
+    Image trainImgs[TRAIN_DATA.SIZE];
+    readImageSet(trainImgs, TRAIN_DATA);
+    transformImageSet(trainImgs, TRAIN_DATA.SIZE, normalize);
 
     // WIP
 
-    // Cleanup
-    printf("Cleaning up...");
+    freeImageSet(trainImgs, TRAIN_DATA.SIZE);
+    /* =========== END OF TRAINING ============== */
+
+    /* =============== TESTING ================== */
+    Image testImgs[TEST_DATA.SIZE];
+    readImageSet(testImgs, TEST_DATA);
+    transformImageSet( testImgs, TEST_DATA.SIZE, normalize);
+
+    // WIP
+
+    freeImageSet(testImgs, TEST_DATA.SIZE);
+    /* =========== END OF TESTING ============== */
+
     freeNeuralNet(&nn);
-    freeImages(trainingImgs, TRAIN_DATA.imgCount);
-    freeImages(testingImgs, TEST_DATA.imgCount);
-    printf(" DONE\n");
 
     return 0;
 }
