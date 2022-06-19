86cfd7d3941f5b64822bf78677ad3696915f9ce8
Jonh Alexis Buot <ja.buotph@gmail.com>
2022-06-19 17:09:00 +0800

    [Feat] Utilized ml library for prepping dataset

--- main.c
+++ main.c
@@ -1,32 +1,37 @@
 #include <stdio.h>
 #include <math.h>
 #include <time.h>
-#include "lib/headers/util.h"
-#include "lib/headers/matrix.h"
-#include "lib/headers/doubly_ll.h"
+#include "lib/headers/stats.h"
 #include "lib/headers/image_set.h"
 #include "lib/headers/neural_net.h"
+#include "lib/headers/ml.h"
 
 int main(int argc, char **argv)
 {
     srand((unsigned int) time(NULL)); // initialize randomizer
 
     LayerDesign layers[] = {{10, INPUT}, {5, HIDDEN}, {4, HIDDEN}, {10, OUTPUT}};
-    NeuralNetOpt opt = { .distStrat = HE_XAVIER, .distSize = 1, .initialBias = 0 };
+    NeuralNetOpt opt = { .distSize = 1, .distStrat = HE, .initialBias = 0, .nodeOrient = ROW, .lr = 0.1 };
     NeuralNetwork nn = createNeuralNet(opt, layers, 4);
 
     /* =============== TRAINING ================== */
     Image trainImgs[TRAIN_DATA.SIZE];
-    readImageSet(trainImgs, TRAIN_DATA);
-    transformImageSet(trainImgs, TRAIN_DATA.SIZE, normalize);
+    readImageSet(trainImgs, TRAIN_DATA.SIZE, TRAIN_DATA);
+    
+    // flatten to node orientation, normalzie values
+    prepDataset(trainImgs, TRAIN_DATA.SIZE, nn.options.nodeOrient, normalize);
+
+    // WIP
 
     freeImageSet(trainImgs, TRAIN_DATA.SIZE);
     /* =========== END OF TRAINING ============== */
 
     /* =============== TESTING ================== */
     Image testImgs[TEST_DATA.SIZE];
-    readImageSet(testImgs, TEST_DATA);
-    transformImageSet(testImgs, TEST_DATA.SIZE, normalize);
+    readImageSet(testImgs, TRAIN_DATA.SIZE, TEST_DATA);
+
+    // flatten to node orientation, normalize values
+    prepDataset(testImgs, TEST_DATA.SIZE, nn.options.nodeOrient, normalize);
 
     // WIP
 
