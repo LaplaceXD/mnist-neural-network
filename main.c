15953cdf9744036312677532b8238646cf186942
Jonh Alexis Buot <ja.buotph@gmail.com>
2022-06-24 00:02:30 +0800

    [Feat] Prepped main.c for network training

--- main.c
+++ main.c
@@ -10,18 +10,20 @@ int main(int argc, char **argv)
 {
     srand((unsigned int) time(NULL)); // initialize randomizer
 
-    LayerDesign layers[] = {{IMG_SIZE, INPUT}, {300, HIDDEN}, {10, OUTPUT}};
-    NeuralNetOpt opt = { .distSize = 1, .distStrat = HE, .initialBias = 0, .nodeOrient = ROW, .lr = 0.1 };
+    LayerDesign layers[] = {{IMG_SIZE, INPUT}, {16, HIDDEN}, {16, HIDDEN}, {10, OUTPUT}};
+    NeuralNetOpt opt = { .distSize = 1, .distStrat = RANDOM, .initialBias = 0, .nodeOrient = ROW, .lr = 0.1 };
     NeuralNetwork nn = createNeuralNet(opt, layers, sizeof(layers) / sizeof(LayerDesign));
 
     /* =============== TRAINING ================== */
     Image trainImgs[TRAIN_DATA.SIZE];
     readImageSet(trainImgs, TRAIN_DATA.SIZE, TRAIN_DATA);
-    
-    // flatten to node orientation, normalzie values
     prepDataset(trainImgs, TRAIN_DATA.SIZE, nn.options.nodeOrient, normalize);
 
-    // WIP
+    int epoch;
+    for(epoch = 1; epoch < 20; epoch++) {
+        printf("EPOCH: %d\n", epoch);
+        networkTrain(nn, sigmoid, 100, trainImgs, TRAIN_DATA.SIZE);
+    }
 
     freeImageSet(trainImgs, TRAIN_DATA.SIZE);
     /* =========== END OF TRAINING ============== */
@@ -29,17 +31,15 @@ int main(int argc, char **argv)
     /* =============== TESTING ================== */
     Image testImgs[TEST_DATA.SIZE];
     readImageSet(testImgs, TEST_DATA.SIZE, TEST_DATA);
-
-    // flatten to node orientation, normalize values
     prepDataset(testImgs, TEST_DATA.SIZE, nn.options.nodeOrient, normalize);
 
-    double acc = networkTest(testImgs, TEST_DATA.SIZE, nn, tanh);
-    printf("Accuracy: %.2lf percent.", acc * 100);
+    double acc = networkTest(nn, sigmoid, testImgs, TEST_DATA.SIZE);
+    printf("\nAccuracy: %.2lf percent.", acc * 100);
 
     freeImageSet(testImgs, TEST_DATA.SIZE);
     /* =========== END OF TESTING ============== */
 
-    // freeNeuralNet(&nn);
+    freeNeuralNet(&nn);
 
     return 0;
 }
